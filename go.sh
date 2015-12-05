c04892018636e876cbbb71cc5d2e0f61dbf2fb37
david.barkhuizen@github.com <david.barkhuizen@gmail.com>
2015-12-05 15:49:53 +0200

    trivial change

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 python3 go.py \
 --data_path /home/david/data/csv_data/equity_indices/^GSPC.csv \
 --out_path /home/david/data/mart/ \
---start_date 2009-03-01 \
---end_date 2015-11-24 \
+--start_date 2015-06-01 \
+--end_date 2015-11-30 \
 --label 'S&P500'
